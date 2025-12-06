d42b44aab511324dc26da87a0b3ed117daf76065
Rahatut Tahrim Mounota <rahatuttahrim@iut-dhaka.edu>
2025-12-06 16:53:33 +0600

    added firebase config
    firebase config file

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,17 +1,26 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: APIKEY,
+  authDomain: AUTHDOM,
+  projectId: PROJID,
+  storageBucket: STGBUCKET,
+  messagingSenderId: MSGSENDERID,
+  appId: APPID,
+  measurementId: MEASUREMENTID
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
 export default app;
+
+
+
+
